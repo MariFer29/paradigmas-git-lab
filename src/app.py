a0811b03444d67fa4fb13dae9c7ca13a29e5bea5
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-08-18 19:16:11 -0600

    feat: implement basic functions and initializes the structure

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from utils import saludo, suma, es_par
+
+def main():
+    print(saludo("Paradigmas"))
+    print("La suma de 3 + 2 es: ", suma(3, 2))
+    print("4 es par? ", es_par(4))
+
+if __name__ == "__main__":
+    main()
+    
